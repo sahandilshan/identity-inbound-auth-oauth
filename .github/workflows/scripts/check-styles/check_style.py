deba5b7ef82d639edebc0fb456ec08926734f9bb
Sahan Dilshan <32576163+sahandilshan@users.noreply.github.com>
2023-11-22 19:39:22 +0530

    Add checkstype workflow

---
+++ .github/workflows/scripts/check-styles/check_style.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from github import Github
+
+# Initialize with the token
+g = Github(os.getenv('GITHUB_TOKEN'))
+repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+pr = repo.get_pull(os.getenv('GITHUB_REF').split('/')[-1])
+
+# Fetch files changed in the PR
+files = pr.get_files()
+
+for file in files:
+    # Only proceed if it's a .java file for example
+    if file.filename.endswith('.java'):
+        diff = file.patch  # The changes in the file
+
+        # Check for full stops at the end of comments
+        for line in diff.split('\n'):
+            if line.startswith('+') and (line.strip().startswith('//') or line.strip().startswith('*')):
+                if not line.strip().endswith('.'):
+                    pr.create_review_comment(
+                        body="Comments should end with a full stop.",
+                        commit_id=file.raw_data['sha'],
+                        path=file.filename,
+                        position=file.raw_data['line']
+                    )
+
+        # Check for extra new lines
+        if '\n+\n+' in diff:
+            pr.create_review_comment(
+                body="Please remove extra new lines.",
+                commit_id=file.raw_data['sha'],
+                path=file.filename,
+                position=file.raw_data['line']
+            )
